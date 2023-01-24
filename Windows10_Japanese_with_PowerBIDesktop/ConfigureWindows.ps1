111bdc23b2149d57ff5481f9965bdd2ae06bfe72
Masahiko Ebisuda <ebibibi@gmail.com>
2023-01-24 10:38:20 +0900

    add sample data download logic

--- Windows10_Japanese_with_PowerBIDesktop/ConfigureWindows.ps1
+++ Windows10_Japanese_with_PowerBIDesktop/ConfigureWindows.ps1
@@ -11,3 +11,16 @@ Set-TimeZone -Id "Tokyo Standard Time"
 # Install PowerBI Desktop
 #########################
 winget install --id=Microsoft.PowerBI --silent
+
+#########################
+# Download Sample Data
+#########################
+if (!(Test-Path("C:\SampleData")))
+{
+    New-Item -ItemType Directory -Path "C:\SampleData"
+}
+
+$sampleExcelUrl = "https://go.microsoft.com/fwlink/?LinkID=521962"
+$sampleExcelFilePath = "C:\SampleData\Financial Sample.xlsx"
+
+Invoke-WebRequest -Uri $sampleExcelUrl -OutFile $sampleExcelFilePath
